7479e6cc2b3dce0bd7a12484cb77e2c4d87fc7a6
davidclaffeygt <142821177+davidclaffeygt@users.noreply.github.com>
2023-09-07 20:27:13 -0400

    add css

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,49 +1,85 @@
 var store = [{
-        "title": "CNN to Lego Construction",
+        "title": "3D Printed Titanium Rocket Motor Closure",
+        "excerpt":"I designed a 3D printed Ti-6Al-4V rocket motor closure in collaboration with Renishaw PLC. The closure was used for a small, end-burning solid rocket motor, which posed unique design requirements. These requirements led to the design of a thin, light-weight, tapered closure that was best suited to manufacturing via additive...","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/3D-print-closure/",
+        "teaser": "/portfolio/assets/images/printed-closure/printed-closure-composite.png"
+      },{
+        "title": "3D Printed Widgets",
+        "excerpt":"A random collection of neat things I’ve designed and printed. Drill Alignment Jig A drill alignment jig with press-fit drill bushings. The two-piece jig clamps around an awkardly shaped part and lets me align the drill bit easily. Worm Gear A fun worm gear desk toy I designed and printed....","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/3D-printed-widgets/",
+        "teaser": "/portfolio/assets/images/3D-printed-widgets/composite.png"
+      },{
+        "title": "Level 3 Rocketry Certification",
+        "excerpt":"Video from an onboard camera on L3 rocket. Overview I designed, built, and flew a high-power rocket as part of the National Association of Rocketry Level 3 Certification program. This certification involves building, flying, and successfully recovering a rocket that safely use an M impulse class or higher commercial solid...","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/L3-rocket/",
+        "teaser": "/portfolio/assets/images/L3-rocket/L3-composite.png"
+      },{
+        "title": "Small, Long Endurance Ceramic Rocket Nozzle Development",
+        "excerpt":"I developed two different nozzle designs to address the challenging requirements for small, long-endurance rocket motors. This work includes: designing and manufacturing a two-piece nozzle design with a 3D printed cellular ceramic insulator using a stereolithography process; designing a single-piece nozzle design with the novel application of a machinable alumina-silicate...","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/ceramic-nozzle-dev/",
+        "teaser": "/portfolio/assets/images/ceramic-nozzle-dev/ceramic-nozzle-dev-composite.png"
+      },{
+        "title": "Combustion Instability Modeling in Solid Rocket Motors",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/portfolio/projects/CNN%20to%20Lego%20Construction/",
-        "teaser": "/portfolio/assets/images/projects/arm.png"
+        "url": "/portfolio/projects/combustion-instabilities/",
+        "teaser": "/portfolio/assets/images/combustion-instability/composite.png"
       },{
-        "title": "Chaotic Pendulum with Dead Reckoning",
+        "title": "Experimental Measurements of Rocket Exhaust Plume Radiant Emission",
+        "excerpt":"Exhaust plume radiant emission is an important performance parameter for solid rocket vehicles where vehicle visibility is a concern. Many motor design parameters influence the plume radiant emission, and understanding these relationships can enable improved design with better plume performance. For my graduate research, I developed and conducted a set...","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/experiments-emission/",
+        "teaser": "/portfolio/assets/images/experiments-emission/composite.png"
+      },{
+        "title": "Multi-Process Manufacturing System Simulator",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/portfolio/projects/Chaotic%20Pendulum%20with%20Dead%20Reckoning/",
-        "teaser": "/portfolio/assets/images/projects/pend.jpg"
+        "url": "/portfolio/projects/manf-process-simulator/",
+        "teaser": "/portfolio/assets/images/manf-process-simulator/composite.png"
       },{
-        "title": "High Speed RC Cars",
+        "title": "End-to-End Differentiable Models and Optimization for Solid Rocket Powered Aircraft with Plume Radiant Emission",
+        "excerpt":"For applications where vehicle visibility is a concern, exhaust plume radiant emission is an important aspect of solid rocket powered vehicle performance. Typical modeling approaches are computationally expensive, and rely on CFD and complicated integration schemes that are not well-suited for fast, iterative vehicle design. To address this gap, I...","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/modeling-emission/",
+        "teaser": "/portfolio/assets/images/modeling-emission/banner.png"
+      },{
+        "title": "Popsicle 3D Printer",
+        "excerpt":"Designed and built in collaboration with Juju Wang and Prashanth Prakash. Description My friends and I built a printer to make edible, frozen popsicles. We started with a Velleman K8200 printer, which uses a bed-XY design. With this printer design, the print head only moves in the Z direction, and...","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/popsicle-printer/",
+        "teaser": "/portfolio/assets/images/popsicle-printer/popsicle-printer-composite.png"
+      },{
+        "title": "Structure and Properties of Titanium and its Alloys",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/portfolio/projects/High%20Speed%20RC%20Experimentation/",
-        "teaser": "/portfolio/assets/images/projects/RC.PNG"
+        "url": "/portfolio/projects/ti-alloys/",
+        "teaser": null
       },{
-        "title": "Analysis of Development Optimal Global Fund Flow",
+        "title": "Low-Thrust Solid Rocket Motors for Small, Fast Aircraft Propulsion: Design and Development",
+        "excerpt":"Matthew T. Vernacchia, Kelly J. Mathesius, and R. John Hansman. “Low-Thrust Solid Rocket Motors for Small, Fast Aircraft Propulsion: Design and Development”. In: Journal of Propulsion and Power 38.1 (2022).   Open access version can be found here.    ","categories": [],
+        "tags": [],
+        "url": "/portfolio/publications/low-thrust-paper/",
+        "teaser": null
+      },{
+        "title": "Strategies for Reuse of Launch Vehicle First Stages",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/portfolio/projects/Holistic%20and%20AI%20Analysis%20of%20Global%20Research%20Efforts%20and%20the%20Common%20Fields/",
-        "teaser": "/portfolio/assets/images/projects/commonsearch.png"
+        "url": "/portfolio/publications/lvreuse/",
+        "teaser": null
       },{
-        "title": "NLP to 3D Mesh Gen Model",
+        "title": "Slow-Burn Ammonium Perchlorate Propellants with Oxamide: Burn Rate Model, Testing, and Applications",
+        "excerpt":"Matthew T. Vernacchia, Kelly J. Mathesius, and R. J. Hansman. “Slow-Burn Ammonium Perchlorate Propellants with Oxamide: Burn Rate Model, Testing, and Applications”. In: Journal of Propulsion and Power 37.5 (2021).   Open access version can be found here.    ","categories": [],
+        "tags": [],
+        "url": "/portfolio/publications/oxamide-paper/",
+        "teaser": null
+      },{
+        "title": "Manufacturing Methods for a Solid Rocket Motor Propelling a Small, Fast Flight Vehicle",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/portfolio/projects/NLP%20to%203D%20Mesh%20Generative%20Model/",
-        "teaser": "/portfolio/assets/images/projects/mesh-generation.png"
-      },{
-        "title": "Peltier Cloud Chamber",
-        "excerpt":"What is a Cloud Chamber? Cloud chambers were used in theoretic physics antiquity to view subatomic particles. They were originally invented by Charles Wilson in 1911 to study cloud formation. Fundamentally all that is required is a saturated vapor such as Isopropyl Alcohol that will ionize as electrically charged particles...","categories": [],
-        "tags": [],
-        "url": "/portfolio/projects/Peltier%20Cooled%20Cloud%20Chamber/",
-        "teaser": "/portfolio/assets/images/finCloud.jpg"
-      },{
-        "title": "High Wavelength SLS",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/portfolio/projects/SLS%20with%20Low%20Wavelength%20Form3%20Laser/",
-        "teaser": "/portfolio/assets/images/projects/SLS.jpg"
-      },{
-        "title": "DART Labs Exoskeleton",
-        "excerpt":"Fall 2021 - Summer 2023 Researched and developed a pipeline that fully processed kinematic data from emg to motion classification. The first stage qualitatively analyzes EMG signals and filters out noisy trials. The second stage uses kinematics and movement vector estimation to segment trials in static, transition, and translation motion...","categories": [],
-        "tags": [],
-        "url": "/portfolio/research/DART/",
-        "teaser": "/portfolio/assets/images/DART/hyperparam2%20-%20Copy.png"
+        "url": "/portfolio/publications/sm-thesis/",
+        "teaser": null
       }]
