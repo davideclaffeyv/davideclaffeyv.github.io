b00873891bc2b25182dfb2021b73004a4af28c35
davidclaffeygt <142821177+davidclaffeygt@users.noreply.github.com>
2023-09-08 13:11:15 -0400

    bundle update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "CNN to Lego Construction",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/CNN%20to%20Lego%20Construction/",
+        "teaser": "/portfolio/assets/images/projects/arm.png"
+      },{
+        "title": "Chaotic Pendulum with Dead Reckoning",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/Chaotic%20Pendulum%20with%20Dead%20Reckoning/",
+        "teaser": "/portfolio/assets/images/projects/pend.jpg"
+      },{
+        "title": "High Speed RC Cars",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/High%20Speed%20RC%20Experimentation/",
+        "teaser": "/portfolio/assets/images/projects/RC.PNG"
+      },{
+        "title": "Analysis of Development Optimal Global Fund Flow",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/Holistic%20and%20AI%20Analysis%20of%20Global%20Research%20Efforts%20and%20the%20Common%20Fields/",
+        "teaser": "/portfolio/assets/images/projects/commonsearch.png"
+      },{
+        "title": "NLP to 3D Mesh Gen Model",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/NLP%20to%203D%20Mesh%20Generative%20Model/",
+        "teaser": "/portfolio/assets/images/projects/mesh-generation.png"
+      },{
+        "title": "Peltier Cloud Chamber",
+        "excerpt":"What is a Cloud Chamber? Cloud chambers were used in theoretic physics antiquity to view subatomic particles. They were originally invented by Charles Wilson in 1911 to study cloud formation. Fundamentally all that is required is a saturated vapor such as Isopropyl Alcohol that will ionize as electrically charged particles...","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/Peltier%20Cooled%20Cloud%20Chamber/",
+        "teaser": "/portfolio/assets/images/finCloud.jpg"
+      },{
+        "title": "High Wavelength SLS",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/SLS%20with%20Low%20Wavelength%20Form3%20Laser/",
+        "teaser": "/portfolio/assets/images/projects/SLS.jpg"
+      },{
+        "title": "DART Labs Exoskeleton",
+        "excerpt":"Fall 2021 - Summer 2023 Researched and developed a pipeline that fully processed kinematic data from emg to motion classification. The first stage qualitatively analyzes EMG signals and filters out noisy trials. The second stage uses kinematics and movement vector estimation to segment trials in static, transition, and translation motion...","categories": [],
+        "tags": [],
+        "url": "/portfolio/research/DART/",
+        "teaser": "/portfolio/assets/images/DART/hyperparam2%20-%20Copy.png"
+      }]
