987d7943005274be142b092b05aea8640fba823e
davideclaffeyv <davidclaffeyv@gmail.com>
2024-09-05 18:20:53 -0400

    resume update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,67 +1,49 @@
 var store = [{
-        "title": "Custom Wood Guitars",
-        "excerpt":"Fall 2023 - Spring 2024 Introduction I helped a close friend of mine at Georgia Tech’s Invention Studio continue a lifelong hobby of his making custom guitars. Koji and I made seven guitar bodies over the course of 2 months. The CNC operation took 3 hours when we started and...","categories": [],
-        "tags": [],
-        "url": "/projects/A_Guitars/",
-        "teaser": "/assets/images/2023Projects/guitar4.png"
-      },{
-        "title": "Peltier Cloud Chamber",
-        "excerpt":"Fall 2023 What is a Cloud Chamber? Cloud chambers were used in theoretic physics antiquity to view subatomic particles. They were originally invented by Charles Wilson in 1911 to study cloud formation. Fundamentally all that is required is a saturated vapor such as Isopropyl Alcohol that will ionize as electrically...","categories": [],
-        "tags": [],
-        "url": "/projects/B_Peltier%20Cooled%20Cloud%20Chamber/",
-        "teaser": "/assets/images/projects/thinCloud.jpg"
-      },{
         "title": "CNN to Lego Construction",
-        "excerpt":"I am in the process of developing a robotic arm that uses an Arduino Mega, Raspberry Pi, AX12a Servos, machined gripper, convolutional neural networks, high-fidelity part imaging, and path planning to process instruction manuals hopefully assemble simple lego sets. So far, I’ve reprogrammed the servos and set up large-packet serial...","categories": [],
+        "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/projects/C_CNN%20to%20Lego%20Construction/",
-        "teaser": "/assets/images/projects/arm.png"
+        "url": "/portfolio/projects/CNN%20to%20Lego%20Construction/",
+        "teaser": "/portfolio/assets/images/projects/arm.png"
       },{
         "title": "Chaotic Pendulum with Dead Reckoning",
-        "excerpt":"Introduction This project was undertaken to further explore controls and chaotic systems. A double pendulum is an example of a chaotic controls system. The goal of this project was to drive a linear rail to balance such a pendulum. This sort of project has been done with various tracking techniques...","categories": [],
+        "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/projects/D_Chaotic%20Pendulum%20with%20Dead%20Reckoning/",
-        "teaser": "/assets/images/projects/modPend.png"
+        "url": "/portfolio/projects/Chaotic%20Pendulum%20with%20Dead%20Reckoning/",
+        "teaser": "/portfolio/assets/images/projects/pend.jpg"
       },{
-        "title": "Grand Challenges Plastic Insulation",
-        "excerpt":"Fall 2020 - Spring 2021 Introduction This year long project was part of Grand Challenges at Georgia Tech. Inspired by mitigating oceanic plastic pollution, this project explored ways in which plastic could be reused as cost effective materials. The primary material explored and tested was as insulation for developing countries...","categories": [],
+        "title": "High Speed RC Cars",
+        "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/projects/E_PlasticInsulation/",
-        "teaser": "/assets/images/projects/plastic.png"
+        "url": "/portfolio/projects/High%20Speed%20RC%20Experimentation/",
+        "teaser": "/portfolio/assets/images/projects/RC.PNG"
       },{
-        "title": "Bioactive Crested Gecko Terrarium",
-        "excerpt":"Crested Geckos Correlophus Ciliatus was thought to be extinct until it was rediscovered in 1994. Originally native to an New Caledonia, Crested Geckos have since been one of the most widely bred gecko species in herpetology and reptile hobbyists. I take care of two of these beautiful animals, ‘Desi’, a...","categories": [],
+        "title": "Analysis of Development Optimal Global Fund Flow",
+        "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/projects/gecko/Bioactive/",
-        "teaser": "/assets/images/gecko/desi.jpg"
+        "url": "/portfolio/projects/Holistic%20and%20AI%20Analysis%20of%20Global%20Research%20Efforts%20and%20the%20Common%20Fields/",
+        "teaser": "/portfolio/assets/images/projects/commonsearch.png"
       },{
-        "title": "Magnetic Computed Tomography",
-        "excerpt":"CT Background Computed Tomography, or CT, is typically in reference to X-ray imaging technology. The fundamental idea begind the technology is that rays of light will interact with different densities of materials uniquely. Denser material will scatter the X-rays, and this “shadow” is then measured. X-rays are used since their...","categories": [],
+        "title": "NLP to 3D Mesh Gen Model",
+        "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/research/A_MCTProjectPage/",
-        "teaser": "/assets/images/MCT/sinogram.png"
+        "url": "/portfolio/projects/NLP%20to%203D%20Mesh%20Generative%20Model/",
+        "teaser": "/portfolio/assets/images/projects/mesh-generation.png"
       },{
-        "title": "Advanced Manufacturing Argon Flow for DED",
-        "excerpt":"Introduction In lieu of the 2022 national AIM grant, the AMPF is automating a large portion of the additive metal machinery for mass physical data collection and processing in order to further study these advanced manufacturing techniques.  The Optomec delivers powdered metals to a weld pool formed by lasers on...","categories": [],
+        "title": "Peltier Cloud Chamber",
+        "excerpt":"What is a Cloud Chamber? Cloud chambers were used in theoretic physics antiquity to view subatomic particles. They were originally invented by Charles Wilson in 1911 to study cloud formation. Fundamentally all that is required is a saturated vapor such as Isopropyl Alcohol that will ionize as electrically charged particles...","categories": [],
         "tags": [],
-        "url": "/research/B_AMPF%20Computer%20Vision%20and%20Additive%20Argon%20Flow/",
-        "teaser": "/assets/images/AMPF/ded_cover.png"
+        "url": "/portfolio/projects/Peltier%20Cooled%20Cloud%20Chamber/",
+        "teaser": "/portfolio/assets/images/finCloud.jpg"
+      },{
+        "title": "High Wavelength SLS",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/projects/SLS%20with%20Low%20Wavelength%20Form3%20Laser/",
+        "teaser": "/portfolio/assets/images/projects/SLS.jpg"
       },{
         "title": "DART Labs Exoskeleton",
         "excerpt":"Fall 2021 - Summer 2023 Researched and developed a pipeline that fully processed kinematic data from emg to motion classification. The first stage qualitatively analyzes EMG signals and filters out noisy trials. The second stage uses kinematics and movement vector estimation to segment trials in static, transition, and translation motion...","categories": [],
         "tags": [],
-        "url": "/research/C_DART/",
-        "teaser": "/assets/images/DART/DARTCOVER.png"
-      },{
-        "title": "LIDAR Athena Wrist Design",
-        "excerpt":"Introduction LIDAR focuses on co-bot robotics, functioning similarly to Boston Dynamics. Several projects study locomotion and manipulation on the MIT Cheetah, Cassie Biped, and Athena. Athena was designed by LIDAR as an upper half of Cassie. In a small team of two, I designed a differential 3 DOF wrist to...","categories": [],
-        "tags": [],
-        "url": "/research/D_LIDAR%20Athena%20Wrist%20Design/",
-        "teaser": "/assets/images/projects/IMG_4203.PNG"
-      },{
-        "title": "Design Build Fly",
-        "excerpt":"Fall 2020 DBF Group I worked on the large model team for a semester, designing an avionic clutch system for a sensor deployment task in the upcoming competition. Wing Manufacturing CAD Wing design based on UIUC airfoil database. Clutch Design Current Spool Design Proposed axially engaged clutch design. The design...","categories": [],
-        "tags": [],
-        "url": "/research/E_Design%20Build%20Fly%20Axial%20Engagement%20Clutch/",
-        "teaser": "/assets/images/photos/IMG_0495.JPG"
+        "url": "/portfolio/research/DART/",
+        "teaser": "/portfolio/assets/images/DART/hyperparam2%20-%20Copy.png"
       }]
